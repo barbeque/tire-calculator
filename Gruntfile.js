43954b52d8e0de415501d0baf2edab096f74019a
mike stedman <github@ravuya.com>
2014-04-06 09:49:41 -0600

    it just needed qunit.js referenced properly from the test html

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,19 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.event.on('qunit.spawn', function(url) {
+    grunt.log.ok('running tests from url ' + url);
+  });
+  grunt.event.on('qunit.begin', function() {
+    grunt.log.ok('beginning qunit');
+  });
+  grunt.event.on('qunit.moduleStart', function(name) {
+    grunt.log.ok('entering module ' + name);
+  });
+  grunt.event.on('qunit.testStart', function(name) {
+    grunt.log.ok('starting test ' + name);
+  });
+
   // Register default tasks
   grunt.registerTask('default', 'qunit');
   // Register travis tasks
