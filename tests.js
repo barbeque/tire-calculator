01063efdca9a32ea70791d28d5d52fa26d79f12a
mike stedman <github@ravuya.com>
2014-04-06 10:02:25 -0600

    An actual functional test, finally

--- tests.js
+++ tests.js
@@ -1,3 +1,16 @@
-test("hello test", function() {
-  ok(1 == "1", "Passed!");
+module("parsing");
+
+module("calculating");
+test("known tire diameter for miata tires", function() {
+  var tireSize = {
+    width: 185,
+    profile: 60,
+    wheelDiameter: 15
+  };
+  var computedDiameter = getDiameterForTireSize(tireSize);
+  closeEnough(computedDiameter, 23.7, 0.1, "94 Miata tires are 23.7 inches tall, but the calculator returned " + computedDiameter);
 });
+
+function closeEnough(provided, expected, tolerance, message) {
+  ok(provided > expected - tolerance && provided < expected + tolerance, message);
+}
