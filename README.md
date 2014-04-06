aac5f3bba01049f33a1751768c07c293a73b8e29
mike stedman <github@ravuya.com>
2014-04-06 08:51:04 -0600

    Code skeleton, no parsing yet

---
+++ README.md
@@ -0,0 +1,11 @@
+Tire Calculator
+===
+A client-side Javascript tire size (diameter) calculator which lets you figure out how far your
+speedometer will be off by running non-factory sized tires.
+
+To use:
+1. Find your stock tire size. Enter it in "original tire size."
+1. Find the tire size your want to compare it to. Enter it in the "new tire size" box.
+1. Press compare.
+
+The general rule of thumb for safety is to stay within 3% of the original speedometer value.
---
+++ tires.js
@@ -0,0 +1,36 @@
+function parseTireSize(input) {
+  // Should be of the form:
+  // xxx yy zz
+  // or Pxxx yy zz
+  // or Pxxx/yy/zz
+  // or xxx-yy-zz etc.
+}
+
+function validateTireSize(tireSize) {
+  // Minimum sizes pulled out of thin air
+  if(!tireSize.wheelDiameter || tireSize.wheelDiameter < 3) {
+    return false;
+  }
+  if(!tireSize.profile || tireSize.profile <= 10) {
+    return false;
+  }
+  if(!tireSize.width || tireSize.width < 80) {
+    return false;
+  }
+
+  return true; // yay
+}
+
+function getDiameterForTireSize(tireSize) {
+  // Convert section width to imperial
+  var widthInches = tireSize.width / 25.4;
+  // Get the diameter for tire size
+  var sidewallHeight = widthInches * (tireSize.profile / 100);
+  var diameter = sidewallHeight * 2 + tireSize.wheelDiameter;
+  return diameter;
+}
+
+function getRollingCircumferenceForTireSize(tireSize) {
+  var diameter = getDiameterForTireSize(tireSize);
+  return Math.PI * diameter;
+}
