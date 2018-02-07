521cf1288f1390b1f9b1ac9cd5548bb60ed51065
mike stedman <barbeque@users.noreply.github.com>
2018-02-07 16:13:29 -0700

    Update predefined tire size list so it doesn't wrap

--- page.js
+++ page.js
@@ -51,7 +51,7 @@ function _loadCarList() {
     { text: "02-05 Subaru WRX", value: "205 55 16" },
     { text: "05-07 Subaru WRX STI", value: "225 45 17" },
     { text: "06-07 Subaru WRX", value: "215 45 17" },
-    { text: "04-08 Subaru Forester XT", value: "215 65 16" },
+    { text: "04-08 Forester XT", value: "215 65 16" },
     { text: "81-89 Lincoln Town Car", value: "215 70 15" }
   ];
 
